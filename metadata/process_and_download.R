8e4ad6586b7770a9b317bbc094b06ce4b1985671
Michael McLaren <m.mclaren42@gmail.com>
2018-04-26 13:25:28 -0400

    Improved Costea2017 data download script
    Changed script to use Aspera Connect instead of wget, for faster speeds.
    Edited the script to detect whether on my local pc or BRC cluster and
    set paths accordingly. Downloaded the full sample data for this project
    from the ENA, which includes the aspera urls and md5 sums.

--- metadata/process_and_download.R
+++ metadata/process_and_download.R
@@ -3,16 +3,16 @@ library(tidyverse)
 library(magrittr)
 
 if (Sys.info()["user"] == "michael") {
-    data_path <- "/home/michael/data/costea2017"
-    git_path <- "/home/michael/active_research/metagenomics_calibration"
-    metadata_path <- file.path(git_path, "costea2017", "metadata")
+    home_path <- "/home/michael"
 } else if (Sys.info()["user"] == "mrmclare") {
-    data_path <- "/home/mrmclare/data/costea2017"
-    git_path <- "/home/mrmclare/metagenomics_calibration"
-    metadata_path <- file.path(git_path, "costea2017", "metadata")
+    home_path <- "/home/mrmclare"
 } else {
     print("User not recognized")
 }
+data_path <- file.path(home_path, "data/costea2017")
+git_path <- file.path(home_path, "active_research/metagenomics_calibration")
+metadata_path <- file.path(git_path, "costea2017", "metadata")
+aspera_path <- file.path(home_path, "applications/aspera/connect")
 
 ## Sample metadata
 # The file sample_description.xlsx is a version of Supplementary Data 3
@@ -23,78 +23,90 @@ phase_dfs <- map(1:3,
 sam_df <- bind_rows(phase_dfs, .id = "Phase")
 ## Sequence data info
 seq_df <- readr::read_tsv(file.path(metadata_path, "PRJEB14847.tsv"))
-
-## Next, connect the two so that I can download the files of the samples I wish
-# Seems like "library_name" is the field that contains the sample name as used
-# in the sample description file for Phase III, but perhaps not for the other
-# phases
-
-# Start with Phase 3, since that's what I'm most interested in and it's
-# easiest to understand.
-ph3 <- seq_df %>% select(library_name, base_count, fastq_bytes, fastq_ftp) %>% 
-    filter(str_detect(library_name, "BYQ"))
-files <- ph3$fastq_ftp %>% str_split(";", simplify = TRUE)
-ph3 <- ph3 %>%
-    mutate(ftp1 = paste0("ftp://", files[, 1]), 
-        ftp2 = paste0("ftp://", files[,2]))
 # Get the (combined for reads1 + reads2) file size of each sample in GB
-file_sizes <- ph3$fastq_bytes %>%
+file_sizes <- seq_df$fastq_bytes %>%
     str_split(";", simplify = TRUE) %>%
     as_tibble %>%
     mutate(V1 = as.numeric(V1), V2 = as.numeric(V2),
         file_size = (V1 + V2) / 1e9) %$%
     file_size
-# Get the sample names from the library names
+seq_df <- seq_df %>%
+    mutate(fastq_GB = file_sizes)
+
+## Next, connect the two so that I can download the files of the samples I wish
+# Start with Phase 3, since that's what I'm most interested in and it's
+# easiest to understand.
+#
+# Seems like "library_name" is the field that contains the sample name as used
+# in the sample description file for Phase III, but perhaps not for the other
+# phases
+ph3 <- seq_df %>%
+    filter(str_detect(library_name, "BYQ"))
 sample_names <- ph3$library_name %>%
     str_match("(.*)_DA") %>%
     {.[,2]}
-# Build a simplied df with Sample info and file info
 ph3 <- ph3 %>%
-    mutate(file_size = file_sizes, Sample = sample_names) %>%
-    select(Sample, file_size, ftp1, ftp2)
-ph3 <- sam_df %>% filter(str_detect(Sample, "BYQ")) %>%
-    left_join(ph3, by = "Sample")
+    mutate(Sample = sample_names) %>%
+    left_join(sam_df, by="Sample")
 
-## Download phase 3 files
-sum(ph3$file_size)
-# A pretty large download. Let's start with just the mock only samples
-mock_ftps <- ph3 %>%
-    filter(Individual == "Mock-only") %$%
-    c(ftp1, ftp2)
-
-# Download the files with rsync
-dir.create(file.path(data_path, "reads"), recursive = TRUE)
-commands <- paste("wget", "-P", file.path(data_path, "reads"), mock_ftps)
+## Download with ascp (aspera connect command line tool)
+asphera_urls <- ph3$fastq_aspera %>% str_split(";", simplify=TRUE) %>% c
+commands <- paste(file.path(aspera_path, "bin/ascp"),
+    "-QT -l 300m -P33001 -i", 
+    file.path(aspera_path, "etc/asperaweb_id_dsa.openssh"),
+    paste0("era-fasp@", asphera_urls),
+    file.path(data_path, "reads")
+    )
 walk(commands, system)
+# system(commands[5])
+
+## Check dowloaded files against md5sums
+downloads <- asphera_urls %>% 
+    str_extract("ERR[0-9]*_[1-2]\\.fastq\\.gz") %>%
+    file.path(data_path, "reads", .)
+md5sums_expected <- ph3$fastq_md5 %>% str_split(";", simplify=TRUE) %>% c
+md5sums_actual <- tools::md5sum(downloads)
+# Fraction of files that were successfully downloaded and so could give an md5
+mean(is.na(md5sums_actual))
+# Check that the downloaded files match the expected md5
+all(md5sums_expected == md5sums_actual, na.rm = TRUE)
+
+# ## Download with wget:
+# ftp_urls <- ph3$fastq_ftp %>% str_split(";", simplify=TRUE) %>% c
+# dir.create(file.path(data_path, "reads"), recursive = TRUE)
+# commands <- paste("wget", "-P", file.path(data_path, "reads"), 
+#     paste0("ftp://", ftp_urls)
+# walk(commands, system)
+
 
 #########################################################################
 ## Poking around the sample data
 
-seq_df %>% names
-seq_df %>% select(experiment_alias, sample_alias, library_name)
-
-seq_df %>% select(experiment_alias, sample_alias, library_name) %>%
-    filter(str_detect(sample_alias, "058"))
-
-seq_df$sample_alias %>% str_subset("BYQ")
-seq_df$library_name %>% str_subset("BYQ")
-
-seq_df %>% select(experiment_alias, sample_alias, library_name) %>%
-    filter(str_detect(library_name, "BYQ"))
-
-seq_df$library_name %>% str_subset("AWF")
-seq_df$sample_alias %>% str_subset("A1")
-
-
-# sample_alias for Phase 1 has a dash, and for phase 2 has an underscore, between the 
-
-ph3 %>% filter(Individual == "Mock-only")
-
-seq_df %>% filter(str_detect(library_name, "BYQ_AAAT"))
-
-seq_df %>% filter(str_detect(library_name, "BYQ")) %>%
-    select(library_name, submitted_ftp)
-
-seq_df %>%
-    select(library_name, submitted_ftp)
-
+# seq_df %>% names
+# seq_df %>% select(experiment_alias, sample_alias, library_name)
+#
+# seq_df %>% select(experiment_alias, sample_alias, library_name) %>%
+#     filter(str_detect(sample_alias, "058"))
+#
+# seq_df$sample_alias %>% str_subset("BYQ")
+# seq_df$library_name %>% str_subset("BYQ")
+#
+# seq_df %>% select(experiment_alias, sample_alias, library_name) %>%
+#     filter(str_detect(library_name, "BYQ"))
+#
+# seq_df$library_name %>% str_subset("AWF")
+# seq_df$sample_alias %>% str_subset("A1")
+#
+#
+# # sample_alias for Phase 1 has a dash, and for phase 2 has an underscore, between the 
+#
+# ph3 %>% filter(Individual == "Mock-only")
+#
+# seq_df %>% filter(str_detect(library_name, "BYQ_AAAT"))
+#
+# seq_df %>% filter(str_detect(library_name, "BYQ")) %>%
+#     select(library_name, submitted_ftp)
+#
+# seq_df %>%
+#     select(library_name, submitted_ftp)
+#
